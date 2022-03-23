c215a6a3264fa3c480d5fbab88ff21ac896492ab
Lena <enelizarova@gmail.com>
2022-03-23 10:09:30 +0300

    fixes

--- js/main.js
+++ js/main.js
@@ -1,110 +1,41 @@
+import {getRandomInteger, getRandomFloat, getRandomElement, getUniqueArr} from './utils.js';
+import {TITLES, TYPES, CHECKTIMES, FEATURES, DESCRIPTIONS, PHOTOS} from './mock.js';
 const CARDS_COUNT = 10;
 
-const TITLES = [
-  'Красивое',
-  'Сарай для вас',
-  'Роскошный эко-шалаш',
-  'Многоэтажка в Воркуте',
-  'Арт-подвал'
-];
-const TYPES = [
-  'palace',
-  'flat',
-  'house',
-  'bungalow',
-  'hotel'
-];
-const CHECKTIME = [
-  '12:00',
-  '13:00',
-  '14:00'
-];
-const FEATURES = [
-  'wifi',
-  'dishwasher',
-  'parking',
-  'washer',
-  'elevator',
-  'conditioner'
-];
-const DESCRIPTION = [
-  'Невероятный вид на рыбов',
-  'Самый лучший сарай на Рублёвке',
-  'Наслажитесь нашими натуральными комарами',
-  'Целая многоэтажка для вашей заполярной тусовки',
-  'Мы приветствуем рисунки на стенах'
-];
-const PHOTOS = [
-  'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/duonguyen-8LrGtIxxa4w.jpg',
-  'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/brandon-hoogenboom-SNxQGWxZQi0.jpg',
-  'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/claire-rendall-b6kAwr1i0Iw.jpg'
-];
-
-const getRandom = (min, max) => {
-  if(min >= max) {
-    throw new Error('Max should be more than min');
-  }
-  return min + Math.random() * (max - min + 1);
-};
-
-const getRandomInt = (min = 0, max = 1) => {
-  if(min < 0 || max < 0) {
-    throw new Error('Only 0 or positive');
-  }
-  return Math.floor(getRandom(min, max));
-};
-
-const getRandomFloatInt = (min = 0, max = 1, digits = 0)  => {
-  if(min < 0 || max < 0) {
-    throw new Error('Only 0 or positive');
-  }
-  return parseFloat(getRandom(min, max).toFixed(digits));
-};
-
-const getRandomArrEl = (els) => els[getRandomInt(0, els.length - 1)];
-
-const getAvatarNumber = () => String(getRandomInt(1, 10)).padStart(2, 0);
-// or
-// const getAvatarNumber = () => {const n = getRandomInt(1, 10); return n < 10 ? `0${n}` : n;};
+const getAvatarNumber = () => String(getRandomInteger(1, 10)).padStart(2, 0);
 
 const getLocation = () => (
-  {lat: getRandomFloatInt(35.65000, 35.70000), lng: getRandomFloatInt(139.70000, 139.80000)}
-);
-const shuffle = (arr) => {
-  const result = arr.slice();
-  for (let i = result.length - 1; i > 0; i--) {
-    const j = Math.floor(Math.random() * (i + 1));
-    [result[i], result[j]] = [result[j], result[i]];
+  {
+    lat: getRandomFloat(35.65000, 35.70000, 5),
+    lng: getRandomFloat(139.70000, 139.80000, 5)
   }
-  return result;
-};
-const getUniqueArr = (arr) => {
-  const randomLength = getRandomInt(1, arr.length);
-  const sortedArr = shuffle(arr);
+);
 
-  return sortedArr.slice(0,randomLength);
-};
+const location = getLocation();
 
 const createCard = () => ({
   author: {
     avatar: `img/avatars/user${getAvatarNumber()}.png`
   },
   offer: {
-    title: getRandomArrEl(TITLES),
-    address: `${getLocation().lat}, ${getLocation().lng}`,
-    price: getRandomInt(0, 13),
-    type: getRandomArrEl(TYPES),
-    rooms: getRandomInt(0, 13),
-    guests: getRandomInt(0, 13),
-    checkin: getRandomArrEl(CHECKTIME),
-    checkout: getRandomArrEl(CHECKTIME),
+    title: getRandomElement(TITLES),
+    address: `${location.lat}, ${location.lng}`,
+    price: getRandomInteger(0, 500000),
+    type: getRandomElement(TYPES),
+    rooms: getRandomInteger(0, 10),
+    guests: getRandomInteger(0, 13),
+    checkin: getRandomElement(CHECKTIMES),
+    checkout: getRandomElement(CHECKTIMES),
     features: getUniqueArr(FEATURES),
-    description: getRandomArrEl(DESCRIPTION),
+    description: getRandomElement(DESCRIPTIONS),
     photos: getUniqueArr(PHOTOS)
   },
-  location: getLocation()
+  location: location
 });
 
-const cards = Array.from({length: CARDS_COUNT}, createCard);
+const getCards = () => Array.from({length: CARDS_COUNT}, createCard);
 
+const cards = getCards();
+
+// eslint-disable-next-line no-console
 console.log(cards);
--- js/utils.js
+++ js/utils.js
@@ -12,15 +12,35 @@ const getRandomInteger = (min = 0, max = 1) => {
   return Math.floor(getRandom(min, max));
 };
 
-const getRandomFloatInteger = (min = 0, max = 1, digits = 0)  => {
+const getRandomFloat = (min = 0, max = 1, digits = 0)  => {
   if(min < 0 || max < 0) {
     throw new Error('Only 0 or positive');
   }
   return parseFloat(getRandom(min, max).toFixed(digits));
 };
 
+const getRandomElement = (elements) => elements[getRandomInteger(0, elements.length - 1)];
+
+const shuffle = (arr) => {
+  const result = arr.slice();
+  for (let i = result.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [result[i], result[j]] = [result[j], result[i]];
+  }
+  return result;
+};
+
+const getUniqueArr = (arr) => {
+  const randomLength = getRandomInteger(1, arr.length);
+  const sortedArr = shuffle(arr);
+
+  return sortedArr.slice(0, randomLength);
+};
+
 export {
   getRandom,
   getRandomInteger,
-  getRandomFloatInteger,
+  getRandomFloat,
+  getRandomElement,
+  getUniqueArr
 };
