8a48405d49beba80d394869f46a353e963493f26
Lena <enelizarova@gmail.com>
2022-04-13 08:33:03 +0300

    return 1 card

--- js/main.js
+++ js/main.js
@@ -1,1 +1,7 @@
-import './similar-card.js';
+import {getCards} from './create-card.js';
+import {similarCard} from './similar-card.js';
+
+const mapTemplate = document.querySelector('#map-canvas');
+const cards = getCards();
+
+mapTemplate.appendChild(similarCard(cards[0]));
--- js/similar-card.js
+++ js/similar-card.js
@@ -1,14 +1,6 @@
-import {getCards} from './create-card.js';
-
 const popupTemplate = document.querySelector('#card')
   .content
   .querySelector('.popup');
-
-const mapTemplate = document.querySelector('#map-canvas');
-
-const similarListFragment = document.createDocumentFragment();
-
-const cards = getCards();
 
 const generateFeatureList = (card, features) => {
   const featuresList = card.querySelector('.popup__features');
@@ -51,35 +43,35 @@ const renderTextNode = ((item, classTtle, value, text = value) => {
   }
 });
 
-cards.forEach(({offer, author}) => {
-  const cardElement = popupTemplate.cloneNode(true);
-  renderTextNode(cardElement, '.popup__title', offer.title);
-  renderTextNode(cardElement, '.popup__text--address', offer.address);
-  renderTextNode(cardElement, '.popup__text--price', offer.price, `${offer.price} ₽/ночь`);
-  renderTextNode(cardElement, '.popup__type', offer.type);
-  renderTextNode(cardElement, '.popup__text--capacity', (offer.rooms && offer.guests), `${offer.rooms} комнаты для ${offer.guests} гостей`);
-  renderTextNode(cardElement, '.popup__text--time', offer.checkin && offer.checkout, `Заезд после ${offer.checkin}, выезд до ${offer.checkout}`);
-  renderTextNode(cardElement, '.popup__description', offer.description);
+const similarCard = ({offer, author}) => {
+  const card = popupTemplate.cloneNode(true);
+  renderTextNode(card, '.popup__title', offer.title);
+  renderTextNode(card, '.popup__text--address', offer.address);
+  renderTextNode(card, '.popup__text--price', offer.price, `${offer.price} ₽/ночь`);
+  renderTextNode(card, '.popup__type', offer.type);
+  renderTextNode(card, '.popup__text--capacity', (offer.rooms && offer.guests), `${offer.rooms} комнаты для ${offer.guests} гостей`);
+  renderTextNode(card, '.popup__text--time', offer.checkin && offer.checkout, `Заезд после ${offer.checkin}, выезд до ${offer.checkout}`);
+  renderTextNode(card, '.popup__description', offer.description);
 
   if(author.avatar) {
-    cardElement.querySelector('.popup__avatar').src = author.avatar;
+    card.querySelector('.popup__avatar').src = author.avatar;
   } else {
-    cardElement.querySelector('.popup__avatar').classList.add('hidden');
+    card.querySelector('.popup__avatar').classList.add('hidden');
   }
 
   if (offer.features) {
-    generateFeatureList (cardElement, offer.features);
+    generateFeatureList (card, offer.features);
   } else {
-    cardElement.querySelector('.popup__features').classList.add('hidden');
+    card.querySelector('.popup__features').classList.add('hidden');
   }
 
   if (offer.photos) {
-    generatePhotos (cardElement, offer.photos);
+    generatePhotos (card, offer.photos);
   } else {
-    cardElement.querySelector('.popup__photos').classList.add('hidden');
+    card.querySelector('.popup__photos').classList.add('hidden');
   }
 
-  similarListFragment.appendChild(cardElement);
-});
+  return card;
+};
 
-mapTemplate.appendChild(similarListFragment.firstChild);
+export {similarCard};
